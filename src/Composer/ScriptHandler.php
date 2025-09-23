5b015c2c6cd93c4cb618253a6083d6ecd1523d76
stephane <soleromel@gmail.com>
2025-09-23 15:34:00 +0200

    fix(script): agent well copied

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -9,14 +9,18 @@ use Symfony\Component\Finder\Finder;
 class ScriptHandler
 {
     private const AGENTS_FILE = 'AGENTS.md';
+    private const PACKAGE_DIRECTORY = '/hypecodeteam/backend-ai-rules';
 
     public static function copyAgents(Event $event): void
     {
+        if (!$event->isDevMode()) {
+            return;
+        }
         $filesystem = new Filesystem();
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
 
-        $source = sprintf('%s/%s', $vendorDir, self::AGENTS_FILE);
-        $target = getcwd();
+        $source = sprintf('%s/%s', $vendorDir . self::PACKAGE_DIRECTORY, self::AGENTS_FILE);
+        $target = getcwd() . '/'. self::AGENTS_FILE;
 
         $filesystem->copy($source, $target, true);
 
